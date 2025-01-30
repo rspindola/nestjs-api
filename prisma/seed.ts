c2a2f88b724b1b63e4d41802c12696e1c01acec5
Renato <rspindola@live.com>
2025-01-29 22:02:08 -0300

    fix: update migration and seed

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -6,35 +6,6 @@ const prisma = new PrismaClient();
 const roundsOfHashing = 10;
 
 async function main() {
-  
-  const createUserPermission = await prisma.permission.upsert({
-    where: { name: 'CREATE_USER' },
-    update: {},
-    create: { name: 'CREATE_USER' },
-  });
-
-  const adminRole = await prisma.role.upsert({
-    where: { name: 'ADMIN' },
-    update: {},
-    create: {
-      name: 'ADMIN',
-      permissions: {
-        connect: [{ id: createUserPermission.id }],
-      },
-    },
-  });
-
-  const userRole = await prisma.role.upsert({
-    where: { name: 'USER' },
-    update: {},
-    create: {
-      name: 'USER',
-      permissions: {
-        connect: [],
-      },
-    },
-  });
-
   const passwordSabin = await bcrypt.hash('password-sabin', roundsOfHashing);
   const passwordAlex = await bcrypt.hash('password-alex', roundsOfHashing);
 
@@ -42,13 +13,11 @@ async function main() {
     where: { email: 'sabin@adams.com' },
     update: {
       password: passwordSabin,
-      roleId: adminRole.id,
     },
     create: {
       email: 'sabin@adams.com',
       name: 'Sabin Adams',
       password: passwordSabin,
-      roleId: adminRole.id,
     },
   });
 
@@ -56,17 +25,15 @@ async function main() {
     where: { email: 'alex@ruheni.com' },
     update: {
       password: passwordAlex,
-      roleId: userRole.id,
     },
     create: {
       email: 'alex@ruheni.com',
       name: 'Alex Ruheni',
       password: passwordAlex,
-      roleId: userRole.id,
     },
   });
 
-  console.log({ user1, user2, adminRole, userRole });
+  console.log({ user1, user2 });
 }
 
 main()
